5978327ad4944e78b553002af8ae8bdba307158f
Professor <rafael.zottesso@ifpr.edu.br>
2025-04-29 11:59:25 -0300

    Criação dos menus e submenus

--- classes.py
+++ classes.py
@@ -1,7 +1,11 @@
+# Importação do conteúdo do framework PeeWee
+# Tipos de campo, Model, conexão com sqlite, etc
 from peewee import *
-# Importar bibliotecas de data e hora para preencher automaticamente
-from datetime import datetime, date 
 
+# Importar bibliotecas de data e hora para preencher automaticamente
+from datetime import datetime 
+
+# Iniciar a conexão com o seu arquivo de banco de dados
 meu_bd = SqliteDatabase("meus_dados.db")
 
 class MinhaBase(Model):
@@ -13,8 +17,14 @@ class Estudante(MinhaBase):
     matricula = CharField(unique=True)
     curso = CharField(default="Técnico em Informática")
 
+    def __str__(self):
+        return self.nome
+
 class TipoProtocolo(MinhaBase):
     nome = CharField()
+
+    def __str__(self):
+        return self.nome
 
 class Protocolo(MinhaBase):
     estudante = ForeignKeyField(Estudante)
@@ -22,7 +32,13 @@ class Protocolo(MinhaBase):
     data_hora = DateTimeField(default=datetime.now)
     tipo = ForeignKeyField(TipoProtocolo)
 
-# Depois de criar todas as suas classes
-# Vamos criar o banco de dados e as tabelas
+    def __str__(self):
+        return f"Procolo: {self.id} - {self.estudante.nome}"
+
+# Depois de criar as classes, conecta no banco de dados
 meu_bd.connect()
+
+# Cria as tabelas para as classes, caso não existam
 meu_bd.create_tables([Estudante, TipoProtocolo, Protocolo])
+
+
--- menu.py
+++ menu.py
@@ -1,9 +1,59 @@
-from classes import Estudante, TipoProtocolo
+from classes import Estudante, TipoProtocolo, Protocolo
+import os
 
-print("Cadastrando Tipo de Protocolo")
+while(True):
+    os.system("cls")
+    print("------------------------")
+    print("---- Menu principal ----")
+    print("------------------------")
+    print("1. Estudante")
+    print("2. Tipos de Protocolo")
+    print("3. Protocolo")
+    opc = int(input("Opção: "))
+    print("------------------------\n")
 
-tipo = input("Tipo de protocolo: ")
-obj1 = TipoProtocolo.create(nome=tipo)
+    if(opc == 1):
+        print("Menu de Estudantes")
+        print("1. Cadastrar")
+        print("2. Listar")
+        print("3. Editar")
+        print("4. Excluir")
+        opc2 = int(input("Opção: "))        
+        # Pula uma linha depois do input
+        print("")
+
+        if(opc2 == 1):
+            print("Cadastrando um estudante...")
+            nome_completo = input("Nome: ")
+            ra = input("Matrícula: ")
+
+            aluno = Estudante.create(nome=nome_completo, matricula=ra)            
+
+            print(f"Estudante {aluno} cadastrado com sucesso!")
+
+            # Só faz o input para ele não voltar automaticamente pra tela inicial e limpar com cls
+            input("\n\nDigite ENTER para continuar...")
+
+        if(opc2 == 2):
+
+            # Selecionar todos os estudantes
+            print("Estudantes cadastrados:")
+
+            # Obter uma lista de objetos do BD
+            lista = Estudante.select()
+            print("\nTotal de estudantes:", len(lista), "\n")
+
+            # Imprime na tela cada estudante
+            for est in lista:
+                print(f"ID: {est.id} - {est.nome} ({est.matricula})")
+
+            # Só faz o input para ele não voltar automaticamente pra tela inicial e limpar com cls
+            input("\n\nDigite ENTER para continuar...")
+
+
+# print("Cadastrando Tipo de Protocolo")
+# tipo = input("Tipo de protocolo: ")
+# obj1 = TipoProtocolo.create(nome=tipo)
 
 print("Tipos de protocolo cadastrados...")
 
@@ -13,23 +63,3 @@ lista = lista.order_by(TipoProtocolo.nome)
 
 for tipo in lista:
     print(tipo.nome)
-
-print("Cadastrando um estudante...")
-nome_completo = input("Nome: ")
-ra = input("Matrícula: ")
-
-est1 = Estudante.create(nome=nome_completo, matricula=ra)
-
-
-# Selecionar todos os estudantes
-print("\n\n")
-print("----- Estudantes cadastrados -----")
-
-# Obter uma lista de objetos do BD
-lista = Estudante.select()
-print("\nTotal de estudantes:", len(lista), "\n")
-
-# Imprime na tela cada estudante
-for objeto in lista:
-    print(f"Nome: {objeto.nome} ({objeto.matricula})")
-
