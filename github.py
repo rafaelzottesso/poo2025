cfcf32bb2f8f879d77f62f0405436794b2e0bd0e
Professor <rafael.zottesso@ifpr.edu.br>
2025-06-10 10:10:59 -0300

    {msg_commit}

--- github.py
+++ github.py
@@ -6,14 +6,18 @@ msg_commit = ""
 while(len(msg_commit)<10):
     msg_commit = input("Mensagem do commit (maior que 10 caracateres): ")
 
+print("\n\nConfigurando email...")
 comando = f"git config user.email \"{email}\""
 os.system(comando)
 
+print("\nAdicionando mudanças e realizando commit...")
 comando = "git add *"
 os.system(comando)
-
 comando = "git commit -m \"{msg_commit}\""
 os.system(comando)
 
+print("\nEnviando ao GitHub. Abra a outra janela para se autenticar...")
 comando = "git push"
 os.system(comando)
+
+print("\nEnvio realizado com sucesso...")
